84bd026c31d96fafaf6db90466f688725be39fb2
zinaric <zinaric@gmail.com>
2023-01-25 13:08:42 +0100

    prva verzija

---
+++ script.js
@@ -0,0 +1,15 @@
+const forma = document.querySelector('form');
+const input = document.querySelector('input');
+const izlaz = document.querySelector('p');
+const img = document.querySelector('img')
+
+forma.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const iznosUEur = parseFloat(input.value);
+    const iznosUKn = iznosUEur * 7.53450;
+    const konacanIznos = iznosUKn.toFixed(2).replace('.', ',');
+    
+    izlaz.innerText = `TO JE ${konacanIznos} HRK!`
+    img.classList.toggle('rotation')
+})
