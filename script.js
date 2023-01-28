6c5dcb9405739db2d789c413293cbaea0c79eabc
zinaric <zinaric@gmail.com>
2023-01-28 11:48:24 +0100

    netočno vrijeme

--- script.js
+++ script.js
@@ -3,6 +3,24 @@ const input = document.querySelector('input');
 const izlaz = document.querySelector('p');
 const img = document.querySelector('img');
 
+////////
+const vrijeme = document.getElementById('random-vrijeme');
+const randomNumber = (max) => {
+    return Math.floor(Math.random(max) * max)
+}
+
+let min;
+
+window.onload = () => {
+    min = randomNumber(60);
+    if (min < 10) {
+        min = `0${min}`
+    } else {
+        min = min;
+    }
+    vrijeme.innerText = `TOČNO VRIJEME ${randomNumber(24)}:${min}`
+}
+
 forma.addEventListener('submit', (e) => {
     e.preventDefault();
 
@@ -12,4 +30,15 @@ forma.addEventListener('submit', (e) => {
     
     izlaz.innerText = `TO JE ${konacanIznos} HRK!`
     img.classList.toggle('rotation')
+
+
+    min = randomNumber(60);
+    if (min < 10) {
+        min = `0${min}`
+    } else {
+        min = min;
+    }
+    vrijeme.innerText = `TOČNO VRIJEME ${randomNumber(24)}:${min}`
 })
+
+
